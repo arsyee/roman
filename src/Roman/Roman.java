545a5f6b28e958c9430c0c5fa73873386f0fe411
orsifelhasznalo <baranya.orsolya@gmail.com>
2017-07-22 22:45:31 +0200

    file fixed to : 5, 10, 50, 100, 500, 1000

--- src/Roman/Roman.java
+++ src/Roman/Roman.java
@@ -7,7 +7,16 @@ public class Roman {
 	}
 
 	public static String intToRoman(int i) {
-		return "I";
+		switch (i) {
+		  case 1 : return "I";
+		  case 5 : return "V";
+		  case 10 : return "X";
+		  case 50 : return "L";
+		  case 100 : return "C";
+		  case 500 : return "D";
+		  case 1000 : return "M";
+		}
+	    return "I";
 	}
 	
 	
