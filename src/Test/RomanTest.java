f8791fb83c52361934715f64a5a72644a9f51e34
arsyee <tibi@fallen.hu>
2017-07-22 22:36:34 +0200

    added all single-letter testcases for intToRoman

--- src/Test/RomanTest.java
+++ src/Test/RomanTest.java
@@ -13,6 +13,10 @@ public class RomanTest {
 		assertEquals("I", Roman.intToRoman(1));
 		assertEquals("V", Roman.intToRoman(5));
 		assertEquals("X", Roman.intToRoman(10));
+		assertEquals("L", Roman.intToRoman(50));
+		assertEquals("C", Roman.intToRoman(100));
+		assertEquals("D", Roman.intToRoman(500));
+		assertEquals("M", Roman.intToRoman(1000));
 	}
 
 }
