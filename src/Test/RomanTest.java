b4add0bde8e1183e79ab163fd387e89de246caa2
orsifelhasznalo <baranya.orsolya@gmail.com>
2017-07-27 11:42:16 +0200

    testing romanToInt 0-3999

--- src/Test/RomanTest.java
+++ src/Test/RomanTest.java
@@ -37,4 +37,60 @@ public class RomanTest {
 		assertEquals("LI", Roman.intToRoman(51));
 		assertEquals("MIII", Roman.intToRoman(1003));
 	}
+	
+	@Test
+	public void testOthers() {
+		assertEquals("XXII", Roman.intToRoman(22));
+		assertEquals("LXIII", Roman.intToRoman(63));
+		assertEquals("LXVI", Roman.intToRoman(66));
+		assertEquals("CXVII", Roman.intToRoman(117));
+		assertEquals("XCIX", Roman.intToRoman(99));
+	}
+	
+	@Test
+	public void testRomanZero() {
+		assertEquals(0, Roman.romanToInt(""));
+	}
+	
+	@Test
+	public void testRomanSingle() {
+		assertEquals(1, Roman.romanToInt("I"));
+		assertEquals(5, Roman.romanToInt(Roman.intToRoman(5)));
+		assertEquals(10, Roman.romanToInt(Roman.intToRoman(10)));
+		assertEquals(50, Roman.romanToInt(Roman.intToRoman(50)));
+		assertEquals(100, Roman.romanToInt(Roman.intToRoman(100)));
+		assertEquals(500, Roman.romanToInt(Roman.intToRoman(500)));
+		assertEquals(1000, Roman.romanToInt(Roman.intToRoman(1000)));
+	}
+	
+	@Test
+	public void testRomanEasy() {
+		assertEquals(12, Roman.romanToInt(Roman.intToRoman(12)));
+		assertEquals(63, Roman.romanToInt(Roman.intToRoman(63)));
+		assertEquals(11, Roman.romanToInt(Roman.intToRoman(11)));
+		assertEquals(58, Roman.romanToInt(Roman.intToRoman(58)));
+		assertEquals(136, Roman.romanToInt(Roman.intToRoman(136)));
+		assertEquals(815, Roman.romanToInt(Roman.intToRoman(815)));
+		assertEquals(2372, Roman.romanToInt(Roman.intToRoman(2372)));
+	}
+	
+	@Test
+	public void testRomanHard() {
+		assertEquals(4, Roman.romanToInt(Roman.intToRoman(4)));
+		assertEquals(9, Roman.romanToInt(Roman.intToRoman(9)));
+		assertEquals(14, Roman.romanToInt(Roman.intToRoman(14)));
+		assertEquals(45, Roman.romanToInt(Roman.intToRoman(45)));
+		assertEquals(99, Roman.romanToInt(Roman.intToRoman(99)));
+		assertEquals(459, Roman.romanToInt(Roman.intToRoman(459)));
+		assertEquals(2009, Roman.romanToInt(Roman.intToRoman(2009)));
+		assertEquals(3899, Roman.romanToInt(Roman.intToRoman(3899)));
+	}
+	
+	@Test
+	public void testRomanAll() {
+		for (int i=0; i<4000; i++) {
+			assertEquals(i, Roman.romanToInt(Roman.intToRoman(i)));
+		}
+	}
+	
 }
